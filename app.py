dbf3fa366942a2513387f919b50051e80879a44b
Andy <agutierrez@umass.edu>
2020-05-20 19:39:26 -0400

    Smalll changes

--- app.py
+++ app.py
@@ -10,5 +10,26 @@ import requests
 
 serverAddr = 'http://127.0.0.1:5000/'
 
+<<<<<<< Updated upstream
 response = requests.get(serverAddr)
 print(response.json())
+=======
+# example GET Request to retrieve data from server
+@app.route('/')
+def get_data():
+    r = requests.get("http://localhost:5000/data")
+    data = json.loads(r.text)
+
+    return render_template('views/index.html', data = data)
+
+
+@app.route('/historical/<n>')
+def get_history(n):
+    r = requests.get("http://localhost:5000/data/"+str(n))
+    data = json.loads(r.text)
+    print(data)
+    return render_template('views/historical.html')
+
+if __name__ == '__main__':
+    app.run(port=5001)
+>>>>>>> Stashed changes
